5328e8b738599c30958fdfce9f2716830c36101b
Erik Badger <erikbadger777@gmail.com>
2020-03-01 14:05:12 -0600

    Cleaned up user provider

--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -6,12 +6,14 @@ import { AsyncStorage } from "react-native";
 
 type User = null | any;
 
-export const UserContext = React.createContext<{
+interface UserState {
     user:User;
     login:(username:string, password:string, client:string) => Promise<void>;
     loginToken:() => Promise<void>;
     logout:() => void;
-}>({
+}
+
+export const UserContext = React.createContext<UserState>({
     user:null,
     login:async () => null,
     loginToken: async() => null,
@@ -19,80 +21,77 @@ export const UserContext = React.createContext<{
 });
 
 export const UserProvider:React.FC = ({ children }) => {
+
     const [ user, setUser ] = React.useState<User>(null);
+
+    const state:UserState = {
+        user:user,
+        async login(username, password, client) {
+
+            const saltRes = await api.get(`users/${username}/salt`);
+            let salt_str:string = saltRes.data;
+
+            var split = salt_str.split(":");
+            var iter = parseInt(split[0]);
+            var salt_base64 = split[1];
+
+            var passbits = sjcl.codec.utf8String.toBits(
+                pcrypt.to_utf8(password)
+            );
+            var salt = sjcl.codec.base64.toBits(salt_base64);
+            let res = pcrypt.pbkdf2(passbits, salt, iter, 64);
+
+            res = sjcl.codec.base64.fromBits(res);
+
+            try {
+                const auth = pcrypt.gen_auth(username, res);
+                api.defaults.headers.common["Authorization"] = auth;
+                var login = await api.post(
+                    `/users/auth/full?domain=${client}`
+                );
+                api.defaults.headers.common["Authorization"] = pcrypt
+                    .to_base64(login.data.user.token);
+
+                AsyncStorage.setItem('token', login.data.user.token);
+                AsyncStorage.setItem('client', client); 
+
+                setUser(login);
+            } catch (error) {
+                console.error(error);
+            }
+
+        },
+        async loginToken() {
+
+            const [token, client] = await Promise.all([
+                AsyncStorage.getItem('token'),
+                AsyncStorage.getItem('client')
+            ]);
+
+            if(!token || !client)return;
+
+            try {
+                api.defaults.headers.common["Authorization"] = pcrypt
+                    .gen_auth(token);
+
+                const login = await api
+                    .post(`/users/auth/full?token=1&domain=${client}`,);
+
+                setUser(login.data);
+            } catch(e) {
+            }
+        },
+        logout() {
+            AsyncStorage.removeItem('token');
+            setUser(null);
+        }
+    }
+
     return (
         <UserContext.Provider
-            value={{
-                user:user,
-                async login(username, password, client) {
-                    const user = await login(username, password, client);
-                    if(user)setUser(user)
-                },
-                async loginToken() {
-                    const user = await loginToken();
-                    if(user)setUser(user);
-                },
-                logout() {
-                    AsyncStorage.removeItem('token');
-                    setUser(null);
-                }
-            }}>
+            value={ state }>
             { children }
         </UserContext.Provider>
     );
-}
-
-const login = async (username:string, password:string, client:string) => {
-    const saltRes = await api.get(`users/${username}/salt`);
-    let salt_str:string = saltRes.data;
-
-    var split = salt_str.split(":");
-    var iter = parseInt(split[0]);
-    var salt_base64 = split[1];
-
-    var passbits = sjcl.codec.utf8String.toBits(
-        pcrypt.to_utf8(password)
-    );
-    var salt = sjcl.codec.base64.toBits(salt_base64);
-    let res = pcrypt.pbkdf2(passbits, salt, iter, 64);
-
-    res = sjcl.codec.base64.fromBits(res);
-
-    try {
-        const auth = pcrypt.gen_auth(username, res);
-        api.defaults.headers.common["Authorization"] = auth;
-        var login = await api.post(
-            `/users/auth/full?domain=${client}`
-        );
-        api.defaults.headers.common["Authorization"] = pcrypt
-            .to_base64(login.data.user.token);
-
-        AsyncStorage.setItem('token', login.data.user.token);
-        AsyncStorage.setItem('client', client); 
-
-        return login;
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
-
-}
-
-const loginToken = async () => {
-
-    const [token, client] = await Promise.all([
-        AsyncStorage.getItem('token'),
-        AsyncStorage.getItem('client')
-    ]);
-
-    if(!token || !client)return false;
-
-    try {
-        api.defaults.headers.common["Authorization"] = pcrypt.gen_auth(token);
-        const login = await api.post(`/users/auth/full?token=1&domain=${client}`,);
-        return login.data;
-    } catch(e) {
-        return false;
-    }
 }
 
