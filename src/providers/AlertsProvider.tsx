ea4fb98e6df86dd294411c41ded5e0ac09ff7991
Erik Badger <erikbadger777@gmail.com>
2020-03-01 14:14:18 -0600

    Cleaned up alerts provider

--- src/providers/AlertsProvider.tsx
+++ src/providers/AlertsProvider.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { UserContext } from "./UserProvider";
 import { api } from "../api";
 
-export const AlertsContext = React.createContext<{
+interface AlertsState {
     alerts:any[];
     fetchAlerts:() => Promise<any>;
-}>({
+}
+
+export const AlertsContext = React.createContext<AlertsState>({
     alerts:[],
     fetchAlerts:async () => null
 });
@@ -13,23 +15,26 @@ export const AlertsContext = React.createContext<{
 export const AlertsProvider:React.FC = ({ children }) => {
     const [ alerts, setAlerts ] = React.useState([]);
     const { user } = React.useContext(UserContext);
+
+    const state = {
+        alerts,
+        async fetchAlerts() {
+            const msid = user.user.marketspaces[0].id;
+
+            const alerts = await api.get(`/email-alert/${msid}`);
+            const active = alerts.data
+                .filter((a:any) => a.is_active);
+
+            setAlerts([
+                ...active.filter((a:any) => a.is_triggered),
+                ...active.filter((a:any) => !a.is_triggered)
+            ])
+        }
+    }
+
     return (
         <AlertsContext.Provider
-            value={{
-                alerts,
-                async fetchAlerts() {
-                    const msid = user.user.marketspaces[0].id;
-
-                    const alerts = await api.get(`/email-alert/${msid}`);
-                    const active = alerts.data
-                        .filter((a:any) => a.is_active);
-
-                    setAlerts([
-                        ...active.filter((a:any) => a.is_triggered),
-                        ...active.filter((a:any) => !a.is_triggered)
-                    ])
-                }
-            }}
+            value={ state }
         >
             { children }
         </AlertsContext.Provider>
