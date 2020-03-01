c9d7580dc4877a092caf0ef2fe94bac2e231333f
Erik Badger <erikbadger777@gmail.com>
2020-02-29 22:37:04 -0600

    added dashboqard screen

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { 
+    Layout,
+    Text
+} from '@ui-kitten/components';
+
+export const DashboardScreen = () => {
+    return (
+        <Layout style={{ flex:1 }}>
+            <Text> Dashboard Screen Works!</Text>
+        </Layout>
+    );
+}
